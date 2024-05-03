dedf99309a02eea2e84081052b7217a950f27642
masgal <dimasrumekso28@gmail.com>
2024-05-03 22:47:29 +0700

    solution4

--- problem4/main.py
+++ problem4/main.py
@@ -3,3 +3,12 @@ Input Harga: 370000
 Input Diskon: 15
 Output: harga yang harus dibayar adalah Rp. 314.500
 '''
+
+inputHarga = input("Input Harga : ")
+inputDiskon = input("Input Diskon : ")
+
+hargaAwal = int(inputHarga)
+diskon = int(inputDiskon)
+hargaDiskon = (diskon/100) * hargaAwal
+hargaAkhir = hargaAwal - hargaDiskon
+print(hargaAkhir)
